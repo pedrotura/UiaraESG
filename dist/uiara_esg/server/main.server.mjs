b4346759f09d126720e5502d2e2e7416f1089fa8
pedrotura <phgt132@gmail.com>
2024-10-28 20:15:51 -0300

    Corrigindo responsividade do formulário

--- dist/uiara_esg/server/main.server.mjs
+++ dist/uiara_esg/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2NX4OAIM.mjs";import"./chunk-QXDVQW4Z.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-TB7GS2DE.mjs";import"./chunk-QXDVQW4Z.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
